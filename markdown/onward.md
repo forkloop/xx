2bb27b661bfba0b1705e283c99d2ce5a198906f2
forkloop <forkloop@gmail.com>
2014-01-15 21:56:32 -0500

    Add timestamp and ignore _ prefixed markdown.

--- markdown/onward.md
+++ markdown/onward.md
@@ -1,3 +1,7 @@
 # Onward
 
 After been in NYC for
+
+```
+if filename prefixed with _, assume it is not published, do not render this file.
+```
--- src/xx/views.clj
+++ src/xx/views.clj
@@ -7,8 +7,12 @@
   (let [basename (clojure.string/replace f ".md" "")]
   [:a {:href basename} (clojure.string/replace basename #"^markdown/" "")]))
 
+; filter out files startsWith `_` as unpublished
+; filter out files not endsWith `.md`
 (defn list-files "list markdown files" []
-  (filter #(.endsWith (.getName %) ".md") (file-seq (io/file "markdown"))))
+  (filter (apply every-pred
+                 [#(.endsWith (.getName %) ".md") #(not (.startsWith (.getName %) "_"))])
+          (file-seq (io/file "markdown"))))
 
 (defn index "index page" []
   (html5
@@ -19,13 +23,20 @@
      [:div
       [:ul (for [f (list-files)] [:li (anchor f)])]]]))
 
+(defn stat "doc-string" [file]
+  (.lastModified (io/file (clojure.string/join "" ["markdown/" file ".md"]))))
+
 (defn markdown "render markdown" [title]
-    (let [content (slurp (clojure.string/join "" ["markdown/" title ".md"]))]
+    (let [content (slurp (clojure.string/join "" ["markdown/" title ".md"]))
+          timestamp (stat title)]
       (html5
         [:head
          [:title title]
          (include-css "/css/screen.css")]
         [:body
-         (md-to-html-string content)])
+         [:a {:href "/"} "index"]
+         [:p {:class "right"} timestamp]
+         [:div
+          (md-to-html-string content)]])
   ;(md-to-html-string content)
   ))
