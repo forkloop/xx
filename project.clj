90f31f53ad3cdfeae6b0aa097da073464d3b74a0
forkloop <forkloop@gmail.com>
2014-03-26 22:16:24 -0400

    Add standalone file.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :plugins [[lein-ring "0.8.8"]
             [generate "0.1.0-SNAPSHOT"]]
   :ring {:handler xx.handler/app}
+  :main xx.cli
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]
---
+++ src/xx/cli.clj
@@ -0,0 +1,5 @@
+(ns xx.cli
+  (:gen-class))
+
+(defn -main "doc-string" [& args]
+  (println "Welcome to xx !"))
