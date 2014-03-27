b9bc292e38bcef281a64358f4926f949745b3231
forkloop <forkloop@gmail.com>
2014-03-26 21:42:08 -0400

    Add plugin 'generate' for creating barebone site.

---
+++ generate/project.clj
@@ -0,0 +1,6 @@
+(defproject generate "0.1.0-SNAPSHOT"
+  :description "Plugin for xx to generate a barebone project."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true)
--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]]
-  :plugins [[lein-ring "0.8.8"]]
+  :plugins [[lein-ring "0.8.8"]
+            [generate "0.1.0-SNAPSHOT"]]
   :ring {:handler xx.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
