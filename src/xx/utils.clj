b049811307074385a311dc3ae62c9d545f35fc68
forkloop <forkloop@gmail.com>
2014-04-24 23:18:38 -0400

    Stop reading full markdown file after getting the tags.

--- src/xx/utils.clj
+++ src/xx/utils.clj
@@ -17,17 +17,20 @@
 
 ;; by convention, the tag is a Clojure hash written at the beginning
 ;; of a markdown file, surrounded by --
-;; FIXME stop reading whole file
 (defn scan-tag "scan for the tag given a file name." [filename]
   (with-open [fd (clojure.java.io/reader filename)]
     (let [tag (atom "")
-          state (atom :seek)]
-    (doseq [line (line-seq fd)]
-      (cond
-        (and (= line "--") (= @state :seek)) (reset! state :read) ;; begin of tag
-        (and (= line "--") (= @state :read)) (reset! state :done) ;; terminate
-        (= @state :read) (reset! tag line)
-        ))
+          state (atom :seek)
+          lines (line-seq fd)]
+      (loop [line (first lines)
+             left (rest lines)]
+        (if (and (not (empty? line)) (not= @state :done))
+          (do
+            (cond
+              (and (= line "--") (= @state :seek)) (reset! state :read) ;; begin of tag
+              (and (= line "--") (= @state :read)) (reset! state :done) ;; terminate
+              (= @state :read) (reset! tag line))
+            (recur (first left) (rest left)))))
       (load-string @tag))))
 
 ; filter out files startsWith `_` as unpublished
